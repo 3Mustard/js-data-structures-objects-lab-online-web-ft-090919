b9df1b6f1cf71adeb87081ed5e233ec6131ae0c7
Matthew C <m23.cullen@gmail.com>
2019-12-25 14:22:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateDriverWithKeyAndValue(driver,key,value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
-    driver['key'] = value;
+    driver[key] = value;
     return driver; 
 }
 
